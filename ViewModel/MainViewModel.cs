db30281a8e15f6743c80a0e5a1e87488c36ad171
SergeyKopko <133692248+SergeyKopko@users.noreply.github.com>
2024-10-23 11:10:24 +0300

    Handlers

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,26 +1,22 @@
-﻿using Autodesk.Revit.DB;
-using System.Collections.ObjectModel;
-using System.Collections;
-using System.Linq;
-using System.Windows.Input;
-using CommunityToolkit.Mvvm.Input;
-using Autodesk.Revit.UI;
-using System.Windows;
+﻿using System;
 using System.Collections.Generic;
-
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using Autodesk.Revit.UI;
+using RevitTest.Interface;
+using RevitTest.Model;
 
 namespace RevitTest.ViewModel
 {
-    public partial class MainViewModel : ViewModelBase
+    public class MainViewModel : ViewModelBase
     {
+        private IPickElementHandler _pickElementHandler;
+        private ExternalEvent _pickElementEvent;
 
-
-        private readonly UIDocument _uidoc;
-
-        public MainViewModel(UIDocument uidoc)
-        {
-            _uidoc = uidoc;
-        }
+        private IChangeElementHandler _changeElementHandler; 
+        private ExternalEvent _changeElementEvent;           
 
         private ObservableCollection<IFamilyViewModel> _revitElements = new ObservableCollection<IFamilyViewModel>();
         public ObservableCollection<IFamilyViewModel> RevitElements
@@ -32,6 +28,57 @@ namespace RevitTest.ViewModel
                 OnPropertyChanged(nameof(RevitElements));
             }
         }
+
+        private ObservableCollection<IFamilyViewModel> _selectedItems = new ObservableCollection<IFamilyViewModel>();
+        public ObservableCollection<IFamilyViewModel> SelectedItems
+        {
+            get => _selectedItems;
+            set
+            {
+                _selectedItems = value;
+                OnPropertyChanged(nameof(SelectedItems));
+            }
+        }
+
+        public ICommand PickCommand { get; }
+        public ICommand ChangeCommand { get; }
+
+        public MainViewModel()
+        {
+            _pickElementHandler = new IPickElementHandler(this);
+            _pickElementEvent = ExternalEvent.Create(_pickElementHandler);
+
+            _changeElementHandler = new IChangeElementHandler(this); // Изменение здесь
+            _changeElementEvent = ExternalEvent.Create(_changeElementHandler); // Изменение здесь
+
+            PickCommand = new RelayCommand(OnPickCommandExecuted);
+            ChangeCommand = new RelayCommand(OnChangeCommandExecuted);
+        }
+
+        private void OnPickCommandExecuted()
+        {
+            _pickElementEvent.Raise();
+        }
+
+        private void OnChangeCommandExecuted()
+        {
+            if (SelectedItems == null)
+            {
+                MessageBox.Show("SelectedItems не инициализирован.");
+                return;
+            }
+
+            if (SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Нет выделенных элементов для изменения.");
+                return;
+            }
+
+            MessageBox.Show($"Количество выделенных элементов: {SelectedItems.Count}");
+
+            _changeElementEvent.Raise();  // Используем внешнее событие для изменения элементов
+        }
+
 
         public void AddRevitElement(IFamilyViewModel element)
         {
@@ -45,68 +92,27 @@ namespace RevitTest.ViewModel
             OnPropertyChanged(nameof(RevitElements));
         }
 
-        private ObservableCollection<IFamilyViewModel> _selectedItems = new ObservableCollection<IFamilyViewModel>();
-        public ObservableCollection<IFamilyViewModel> SelectedItems
+        public class RelayCommand : ICommand
         {
-            get => _selectedItems;
-            set
-            {
-                _selectedItems = value;
+            private readonly Action _execute;
+            private readonly Func<bool> _canExecute;
+            public event EventHandler CanExecuteChanged;
 
-                OnPropertyChanged(nameof(SelectedItems));
-                
+            public RelayCommand(Action execute, Func<bool> canExecute = null)
+            {
+                _execute = execute;
+                _canExecute = canExecute;
+            }
+
+            public bool CanExecute(object parameter) => _canExecute?.Invoke() ?? true;
+
+            public void Execute(object parameter) => _execute?.Invoke();
+
+            public void RaiseCanExecuteChanged()
+            {
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
             }
         }
-
-        public ICommand ChangeWindowSizeCommand => new RelayCommand(ChangeWindowSize);
-
-        private void ChangeWindowSize()
-        {
-            var selectedItems = SelectedItems.ToList();
-
-            if (selectedItems.Count == 0)
-            {
-                MessageBox.Show("Нет выделенных элементов для изменения размера.");
-                return;
-            }
-            using (var transaction = new Transaction(_uidoc.Document, "Изменение размеров окон"))
-            {
-                transaction.Start();
-                foreach (var item in selectedItems)
-                {
-                    if (item is WindowFamilyViewModel windowViewModel)
-
-                    {
-                        var window = _uidoc.Document.GetElement(item.Id) as FamilyInstance;
-
-
-
-                        if (window == null)
-                        {
-                            MessageBox.Show($"Окно с ID {item.Id} не найдено.");
-                            continue;
-                        }
-
-                        var widthWindow = window.get_Parameter(BuiltInParameter.WINDOW_WIDTH);
-                        var heightWindow = window.get_Parameter(BuiltInParameter.WINDOW_HEIGHT);
-
-                        if (widthWindow == null || heightWindow == null)
-                        {
-                            continue;
-                        }
-                        double newHeight = heightWindow.AsDouble() * 2;
-                        double newWidth = widthWindow.AsDouble() * 2;
-
-                        widthWindow.SetValueString(newWidth.ToString());
-                        heightWindow.SetValueString(newHeight.ToString());
-                    }
-                }
-
-                transaction.Commit();
-            }
-        }
-
-
 
     }
 }
